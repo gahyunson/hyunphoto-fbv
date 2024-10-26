af29099ab863d61340678e738ec522c1e6a206dc
GH <chelsey.gahyun@gmail.com>
2024-10-26 16:11:23 +0900

    blank modify

--- app/photos/views.py
+++ app/photos/views.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 
 from core.models import Photos, Prices
 from photos import serializers
